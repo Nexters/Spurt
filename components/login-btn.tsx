a9d4c6284a24718c8da7c006bc6acce47243172b
minkukjo <strike0115@naver.com>
2023-07-09 13:22:16 +0900

    구글 로그인 기능 추가

---
+++ components/login-btn.tsx
@@ -0,0 +1,33 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
+
+export default function LoginBtn() {
+   const { data: session } = useSession();
+
+   if (session) {
+      return (
+         <>
+            Signed in as {session.user !== undefined ? session.user.email : 'Not found User'}
+            <br></br>
+            <img src={session.user!.image!}></img>
+            <br />
+            <br />
+            <button
+               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+               onClick={() => signOut()}>
+               Sign out
+            </button>
+         </>
+      );
+   }
+   return (
+      <>
+         Not signed in <br />
+         <br />
+         <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={() => signIn()}>
+            Sign in
+         </button>
+      </>
+   );
+}
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-export default NextAuth({
+export const authOptions = {
    providers: [
       // OAuth authentication providers...
       GoogleProvider({
@@ -9,4 +9,6 @@ export default NextAuth({
          clientSecret: process.env.GOOGLE_SECRET,
       }),
    ],
-});
+};
+
+export default NextAuth(authOptions);
