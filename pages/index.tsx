0e83b65e97767840e6f1bcfbca2af56f8aaca26c
minkukjo <strike0115@naver.com>
2023-07-15 17:45:43 +0900

    CSS 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import LoginBtn from '@/components/login-btn';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 export default function Home() {
    return (
       <div>
          <LoginBtn></LoginBtn>
-         <div className="my-5"></div>
+         <div className="my-5 text-title1">가나다라마바사</div>
+         <div></div>
          <Link href="/search" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
             이동하기
          </Link>
