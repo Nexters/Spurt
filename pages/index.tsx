a2d07429d85e33c0ae4fe83f558afa92993a1369
narinn-star <skfls2618@naver.com>
2023-07-17 23:03:17 +0900

    global css 가운데 정렬 및 가로 px 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
-import Header from '@/components/header';
-import LoginBtn from '@/components/login-btn';
-import { useSession } from 'next-auth/react';
+import Header from "@/components/header";
+import LoginBtn from "@/components/login-btn";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-   const { status, data: session } = useSession();
+  const { status, data: session } = useSession();
 
-   const isLogin = !!session && status === 'authenticated';
-   const token = isLogin ? session.access_token : '';
+  const isLogin = !!session && status === "authenticated";
+  const token = isLogin ? session.access_token : "";
 
-   return (
-      <div>
-         <Header></Header>
-         <LoginBtn></LoginBtn>
-         <div>{token}</div>
-      </div>
-   );
+  return (
+    <div className="container">
+      <Header></Header>
+      <LoginBtn></LoginBtn>
+      <div>{token}</div>
+    </div>
+  );
 }
