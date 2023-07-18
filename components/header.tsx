ee940d77d6b3e0a4e1c3bf116d47f800c7ebc349
minkukjo <strike0115@naver.com>
2023-07-18 23:47:10 +0900

    헤더 수정

--- components/header.tsx
+++ components/header.tsx
@@ -1,51 +1,41 @@
-import { useSession } from "next-auth/react";
-import Title from "../img/Frame 2609495.svg";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import Image from "next/image";
+import { useSession } from 'next-auth/react';
+import Title from '../img/Frame 2609495.svg';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
-  const { data: session } = useSession();
-  const router = useRouter();
+   const { data: session } = useSession();
+   const router = useRouter();
 
-  return (
-    <div className="w-full justify-center justify-items-center grid">
-      <div className="flex flex-row w-560 h-32">
-        <div className="grid content-center mr-9">
-          <Title></Title>
-        </div>
-        <div className="flex flex-row items-center">
-          <div className="w-72 flex justify-between">
-            <Link
-              href="/"
-              className={`${router.pathname === "/" ? "font-bold" : ""}`}
-            >
-              질문 모음
-            </Link>
-            <Link
-              href="/note"
-              className={`${router.pathname === "/note" ? "font-bold" : ""}`}
-            >
-              10분 노트
-            </Link>
-            <Link href="/project">나의 프로젝트</Link>
-            <Link href="/signin">로그인</Link>
-          </div>
-        </div>
-        <div className="flex flex-row items-center ml-24">
-          {session ? (
-            <Image
-              src={session.user!.image!}
-              width="64"
-              height="64"
-              className="rounded-full"
-              alt="프로필 사진"
-            ></Image>
-          ) : (
-            ""
-          )}
-        </div>
+   return (
+      <div className="flex flex-row h-32 justify-between">
+         <div className="grid content-center">
+            <Title></Title>
+         </div>
+         <div className="flex flex-row items-center">
+            <div className="flex justify-between">
+               <Link href="/" className={`${router.pathname === '/' ? 'font-bold' : ''} mr-7`}>
+                  질문 모음
+               </Link>
+               <Link href="/note" className={`${router.pathname === '/note' ? 'font-bold' : ''} mr-7`}>
+                  10분 노트
+               </Link>
+               <Link href="/project">나의 프로젝트</Link>
+            </div>
+         </div>
+         <div className="flex flex-row items-center w-16">
+            {session ? (
+               <Image
+                  src={session.user!.image!}
+                  width="64"
+                  height="64"
+                  className="rounded-full"
+                  alt="프로필 사진"></Image>
+            ) : (
+               ''
+            )}
+         </div>
       </div>
-    </div>
-  );
+   );
 }
