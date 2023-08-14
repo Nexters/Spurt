fd351dfd7eea4c40dd0765c481efc373d36d4e3e
minkukjo <strike0115@naver.com>
2023-08-15 02:38:37 +0900

    질문 수정

--- components/pc/Keywords/Questions/QuestionCard.tsx
+++ components/pc/Keywords/Questions/QuestionCard.tsx
@@ -5,8 +5,8 @@ import { useRouter } from 'next/router';
 
 const QuestionCard = ({ subject }: RandomQuestion) => {
   const router = useRouter();
-  const session = useSession();
-  const path = session ? '/post' : '/signin';
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-col">
       <div className="text-heading2 text-gray-700 w-[226px] h-[169px] py-[22px] px-[20px] flex bg-gray-100 border border-gray_line rounded-t-2xl">
@@ -15,7 +15,10 @@ const QuestionCard = ({ subject }: RandomQuestion) => {
       <button
         className="flex justify-center items-center h-[71px] text-body5 text-gray-500 text-center gap-1 self-stretch bg-gray-100 border-x border-b border-gray_line rounded-b-2xl"
         onClick={() => {
-          router.push({ pathname: path, query: { subject } });
+          router.push({
+            pathname: session ? '/post' : '/signin',
+            query: session ? { subject } : {},
+          });
         }}
       >
         답변 쓰기 <PlusIcon />
